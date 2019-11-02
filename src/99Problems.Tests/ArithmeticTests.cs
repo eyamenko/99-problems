f71d8755516bc52da26552b669277ccfd4bf7df1
Eugene Yamenko <yamenko@me.com>
2019-11-02 11:45:15 +1100

    2.02

--- src/99Problems.Tests/ArithmeticTests.cs
+++ src/99Problems.Tests/ArithmeticTests.cs
@@ -23,6 +23,18 @@ namespace _99Problems.Tests
             yield return new object[] { 20, false, };
         }
 
+        private static IEnumerable<object[]> _2_02_data()
+        {
+            yield return new object[] { 0, new List<int>() };
+            yield return new object[] { 1, new List<int>() };
+            yield return new object[] { 2, new List<int> { 2 } };
+            yield return new object[] { 3, new List<int> { 3 } };
+            yield return new object[] { 4, new List<int> { 2, 2 } };
+            yield return new object[] { 5, new List<int> { 5 } };
+            yield return new object[] { 20, new List<int> { 2, 2, 5 } };
+            yield return new object[] { 315, new List<int> { 3, 3, 5, 7 } };
+        }
+
         [DataTestMethod]
         [DynamicData(nameof(_2_01_data), DynamicDataSourceType.Method)]
         public void _2_01_should_determine_whether_given_integer_number_is_prime(int number, bool expected)
@@ -30,6 +42,15 @@ namespace _99Problems.Tests
             var actual = Arithmetic._2_01(number);
 
             Assert.AreEqual(expected, actual);
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(_2_02_data), DynamicDataSourceType.Method)]
+        public void _2_02_should_determine_prime_factors_of_given_positive_integer(int number, List<int> expected)
+        {
+            var actual = Arithmetic._2_02(number);
+
+            CollectionAssert.AreEqual(expected, actual);
         }
     }
 }
--- src/99Problems/Arithmetic.cs
+++ src/99Problems/Arithmetic.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace _99Problems
@@ -13,6 +14,32 @@ namespace _99Problems
         public static bool _2_01(int number)
         {
             return number > 1 && Enumerable.Range(2, Convert.ToInt32(Math.Round(Math.Sqrt(number))) - 1).All(n => number % n != 0);
+        }
+
+        /// <summary>
+        /// Determine the prime factors of a given positive integer. Construct a flat list containing the prime factors in ascending order.
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns></returns>
+        public static List<int> _2_02(int number, List<int> result = null)
+        {
+            result ??= new List<int>();
+
+            if (number < 2)
+            {
+                return result;
+            }
+
+            var primeFactor = Enumerable.Range(2, number - 1).Where(_2_01).FirstOrDefault(n => number % n == 0);
+
+            if (primeFactor == default)
+            {
+                return result;
+            }
+
+            result.Add(primeFactor);
+
+            return _2_02(number / primeFactor, result);
         }
     }
 }
